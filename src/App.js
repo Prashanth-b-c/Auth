4cc876658a16b82a5f6deda0b26c1ec68d941123
Prashanth-b-c <87267732+Prashanth-b-c@users.noreply.github.com>
2022-04-10 22:21:47 +0530

    react-auth

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
+import NavBar from './Auth/NavBar';
+
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Auth/AppBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './appbar.css'
+
+const AppBar = () => {
+
+    return (
+        <div className='app1' >
+            <div className='header'>
+                <div className='homepage-appbar'>
+                    <h1>Authentication</h1>
+                    <ul className='nav-links'>
+                        
+                        <li><Link to='/login'>Login</Link></li>
+                        <li><Link to='/register'>Register</Link></li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <img src='https://www.kindpng.com/picc/m/273-2738804_php-login-and-authentication-login-hd-png-download.png'></img>
+            </div>
+        </div>
+    )
+}
+
+export default AppBar
